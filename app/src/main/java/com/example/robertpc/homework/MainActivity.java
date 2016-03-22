5a9386e963fd49b28443f423d8aa456b29022a96
srengkhorn <srengkhorn@gmail.com>
2016-03-22 16:47:46 +0700

    add slide animation

--- app/src/main/java/com/example/robertpc/homework/MainActivity.java
+++ app/src/main/java/com/example/robertpc/homework/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.robertpc.homework;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -75,38 +72,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent in = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(in);
-                overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
+                overridePendingTransition(R.anim.enter_from_left, R.anim.exit_to_right);
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/robertpc/homework/RegisterActivity.java
+++ app/src/main/java/com/example/robertpc/homework/RegisterActivity.java
@@ -2,10 +2,9 @@ package com.example.robertpc.homework;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,16 +22,8 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         bRegister = (Button) findViewById(R.id.btn_submit);
         tEmail = (EditText) findViewById(R.id.editTxtEmail);
@@ -43,13 +34,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String uEmail = tEmail.getText().toString();
                 String uPass = tPass.getText().toString().trim();
                 String uCon = tConfirm.getText().toString().trim();
 
                 AlertDialog.Builder alertMessage = new AlertDialog.Builder(RegisterActivity.this, R.style.MyAlertDialogStyle);
 
-                if(uEmail.matches("") || uPass.matches("") || uCon.matches("")){
+                if(tEmail.getText().toString().matches("") || uPass.matches("") || uCon.matches("")){
 
 
                     alertMessage.setTitle("Oop !")
@@ -71,12 +61,11 @@ public class RegisterActivity extends AppCompatActivity {
                 }else {
 
                     User user = new User();
-                    user.setEmail(uEmail);
+                    user.setEmail(tEmail.getText().toString());
                     user.setPassword(uPass);
-                    Toast.makeText(getApplicationContext(), "Saved!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Saved! " + user.getEmail(), Toast.LENGTH_SHORT).show();
 
                 }
-
 
             }
         });
@@ -84,9 +73,19 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                super.onBackPressed();
+                overridePendingTransition(R.anim.enter_right, R.anim.exit_left);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onBackPressed() {
         super.onBackPressed();
-        overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
+        overridePendingTransition(R.anim.enter_right, R.anim.exit_left);
     }
 
 }
