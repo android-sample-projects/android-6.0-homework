330ba46a086b17f80ae545f80f003cec23778506
srengkhorn <srengkhorn@gmail.com>
2016-03-25 11:52:40 +0700

    update

--- app/src/main/java/com/example/robertpc/homework/MainActivity.java
+++ app/src/main/java/com/example/robertpc/homework/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
--- app/src/main/java/com/example/robertpc/homework/RegisterActivity.java
+++ app/src/main/java/com/example/robertpc/homework/RegisterActivity.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
     Button bRegister;
@@ -66,6 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
                 }else {
 
                     User user = new User();
+
                     user.setEmail(uEmail);
                     user.setPassword(uPass);
 
